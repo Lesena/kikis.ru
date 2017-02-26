bfc35912fa0253c2a83c9acf3baf67a65745b46d
Lesena13 <kravchenkol@i.ua>
2017-02-26 14:19:34 +0200

    Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+
+    $('.profile').bind('click', function() {
+    $('.detail').addClass('active');});
+    $('.close').bind('click', function() {
+        $('.detail').removeClass('active');});
+});
